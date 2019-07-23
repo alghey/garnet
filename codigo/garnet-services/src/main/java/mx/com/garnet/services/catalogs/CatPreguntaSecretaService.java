45b7269c1a8906e075b548ce6856d646550de9a1
Alejandra Ugalde <alejandra.ugalde@serti.com.mx>
2019-07-23 14:07:31 -0500

    Se agregó el repositorio de Especialidad y también su test. Se eliminaron unas clases que no servian jajaja.

--- codigo/garnet-services/src/main/java/mx/com/garnet/services/catalogs/CatPreguntaSecretaService.java
+++
@@ -1,7 +0,0 @@
-package mx.com.garnet.services.catalogs;
-
-import mx.com.garnet.common.response.catalogs.CatPreguntaSecretaResponse;
-
-public interface CatPreguntaSecretaService {
-    CatPreguntaSecretaResponse obtenerPreguntasSecretas();
-}
---
+++ codigo/garnet-services/src/test/java/mx/com/garnet/test/EspecialidadRepositoryTest.java
@@ -0,0 +1,62 @@
+package mx.com.garnet.test;
+
+import mx.com.garnet.persistence.entities.CatEspecialidad;
+import mx.com.garnet.persistence.repository.EspecialidadRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.Objects;
+import java.util.Optional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ComponentScan("mx.com.garnet")
+@AutoConfigureMockMvc
+@TestPropertySource(
+        locations = "classpath:application-integrationtest.properties")
+@ActiveProfiles("STANDALONE")
+public class EspecialidadRepositoryTest {
+
+    @Autowired
+    EspecialidadRepository especialidadRepository;
+
+    //@Test
+    public void listAllEspecialidad(){
+
+        Iterable<CatEspecialidad> especialidades = especialidadRepository.findAll();
+
+        especialidades.forEach(especialidad->{
+            System.out.println("Id " + especialidad.getIdEspecialidad());
+            System.out.println("Nombre " + especialidad.getNombre());
+        });
+    }
+
+    @Test
+    public void findByIdEspecialidad(){
+
+        Optional<CatEspecialidad> especialidad = especialidadRepository.findById(20);
+/*
+        try{
+            System.out.println("Id ");
+            System.out.println("Nombre " + especialidad.get().getNombre());
+        }catch (Exception e){
+            System.out.println("No se encontraron registros " + e);
+        }
+*/
+
+       if(especialidad.isPresent()){
+            System.out.println("Id " + especialidad.get().getIdEspecialidad());
+            System.out.println("Nombre " + especialidad.get().getNombre());
+        }else{
+            System.out.println("No se encontraron registros");
+        }
+    }
+
+}
